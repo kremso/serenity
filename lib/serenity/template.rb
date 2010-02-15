3a2abe812eb1c6d4b9bf9470011c21a01db2475d
Tomas Kramar <kramar.tomas@gmail.com>
2010-02-15 23:05:41 +0100

    Fixed the tempfile problem

--- lib/serenity/template.rb
+++ lib/serenity/template.rb
@@ -16,7 +16,7 @@ module Serenity
         odteruby = OdtEruby.new(XmlReader.new(content), true)
         out = odteruby.evaluate context
 
-        file = Tempfile.open("serenity_#{rand(1000000)}.xml") do |f|
+        file = Tempfile.new("serenity_#{rand(1000000)}.xml") do |f|
           f << out
         end
 

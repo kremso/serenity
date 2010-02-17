0dbdda15f6cac0d5373ef1526e6fe057db800e49
Tomas Kramar <kramar.tomas@gmail.com>
2010-02-17 22:46:04 +0100

    Added showcase document, sample script and output

---
+++ showcase/showcase.rb
@@ -0,0 +1,29 @@
+$:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+
+require 'serenity'
+
+class Showcase
+  include Serenity::Generator
+
+  Person = Struct.new(:name, :items)
+  Item = Struct.new(:name, :usage)
+
+  def generate_showcase
+    @title = 'Serenity inventory'
+
+    mals_items = [Item.new('Moses Brothers Self-Defense Engine Frontier Model B', 'Lock and load')]
+    mal = Person.new('Malcolm Reynolds', mals_items)
+
+    jaynes_items = [Item.new('Vera', 'Callahan full-bore auto-lock with a customized trigger, double cartridge and thorough gauge'),
+                    Item.new('Lux', 'Ratatata'),
+                    Item.new('Knife', 'Cut-throat')]
+    jayne = Person.new('Jayne Cobb', jaynes_items)
+
+    @crew = [mal, jayne]
+
+    render_odt 'showcase.odt'
+  end
+end
+
+showcase = Showcase.new
+showcase.generate_showcase

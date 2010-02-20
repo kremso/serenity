320aba64740fadcb2a993e6af0fa0e9e62e41887
Tomas Kramar <kramar.tomas@gmail.com>
2010-02-20 11:15:36 +0100

    generator_spec cleans up after itself

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -2,6 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 module Serenity
   describe Generator do
+    after(:each) do
+      FileUtils.rm(fixture('loop_output.odt'))
+    end
+
     it 'should make context from instance variables and run the provided template' do
       class GeneratorClient
         include Generator

d39f4dc87feb0c23ee1b4efc357afee49172a682
Tomas Kramar <kramar.tomas@gmail.com>
2010-09-22 21:18:13 +0200

    Templates use instance instead of local variables
    This also fixes ruby 1.9 incompatibilities.
    I don't know why I chose to convert instance variables to locals
    in the first place, considering that rails uses instance variables
    in its templates.

--- lib/serenity/generator.rb
+++ lib/serenity/generator.rb
@@ -1,22 +1,11 @@
 module Serenity
   module Generator
     def render_odt template_path, output_path = output_name(template_path)
-
-      local_variables = {}
-      instance_variables.each { |name| local_variables[name] = instance_variable_get(name) }
-
-      locals = instance_variables.map { |name| "#{ival_name_to_local(name)} = local_variables['#{name}'];" }.join
-      eval locals
-
       template = Template.new template_path, output_path
       template.process binding
     end
 
     private
-
-    def ival_name_to_local ival_name
-      ival_name[1, ival_name.length]
-    end
 
     def output_name input
       if input =~ /(.+)\.odt\Z/
--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -10,43 +10,43 @@ module Serenity
     end
 
     it "should process a document with simple variable substitution" do
-      name = 'Malcolm Reynolds'
-      title = 'captain'
+      @name = 'Malcolm Reynolds'
+      @title = 'captain'
 
       template = Template.new(fixture('variables.odt'), 'output_variables.odt')
       lambda {template.process binding}.should_not raise_error
     end
 
     it "should unroll a simple for loop" do
-      crew = %w{'River', 'Jayne', 'Wash'}
+      @crew = %w{'River', 'Jayne', 'Wash'}
 
       template = Template.new(fixture('loop.odt'), 'output_loop.odt')
       lambda {template.process binding}.should_not raise_error
     end
 
     it "should unroll an advanced loop with tables" do
-      ships = [Ship.new('Firefly', 'transport'), Ship.new('Colonial', 'battle')]
+      @ships = [Ship.new('Firefly', 'transport'), Ship.new('Colonial', 'battle')]
 
       template = Template.new(fixture('loop_table.odt'), 'output_loop_table.odt')
       lambda {template.process binding}.should_not raise_error
     end
 
     it "should process an advanced document" do
-      persons = [Person.new('Malcolm', 'captain'), Person.new('River', 'psychic'), Person.new('Jay', 'gunslinger')]
+      @persons = [Person.new('Malcolm', 'captain'), Person.new('River', 'psychic'), Person.new('Jay', 'gunslinger')]
 
       template = Template.new(fixture('advanced.odt'), 'output_advanced.odt')
       lambda {template.process binding}.should_not raise_error
     end
 
     it "should loop and generate table rows" do
-      ships = [Ship.new('Firefly', 'transport'), Ship.new('Colonial', 'battle')]
+      @ships = [Ship.new('Firefly', 'transport'), Ship.new('Colonial', 'battle')]
 
       template = Template.new(fixture('table_rows.odt'), 'output_table_rows.odt')
       lambda {template.process binding}.should_not raise_error
     end
 
     it "should parse the header" do
-      title = 'captain'
+      @title = 'captain'
 
       template = Template.new(fixture('header.odt'), 'output_header.odt')
       template.process(binding)
